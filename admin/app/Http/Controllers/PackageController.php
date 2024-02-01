0bf913d9679f6aed7e7d301d3b15484e73714f0f
in-th3-l00p <indianucode0303@gmail.com>
2024-02-01 11:37:14 +0200

    added packages on admin panel

--- admin/app/Http/Controllers/PackageController.php
+++ admin/app/Http/Controllers/PackageController.php
@@ -3,60 +3,119 @@
 namespace App\Http\Controllers;
 
 use App\Models\Package;
+use App\Models\Price;
 use Illuminate\Http\Request;
+use Stripe\Exception\ApiErrorException;
+use Stripe\Product;
+use Stripe\StripeClient;
 
 class PackageController extends Controller
 {
-    public function index()
-    {
-        //
+    public function index() {
+        return view("packages.index", [
+            "packages" => Package::all()
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function create(StripeClient $stripe, Request $request) {
+        return view("packages.create", [
+            "availableProducts" =>
+                collect($stripe->products->all()->data)
+                    ->filter(fn (Product $product) =>
+                        $product->active &&
+                        isset($product->default_price)
+                    )
+                    ->map(function (Product $product) use ($stripe) {
+                        if (gettype($product->default_price) === "string")
+                            $product->default_price = $stripe
+                                ->prices
+                                ->retrieve($product->default_price);
+                        return $product;
+                    })
+                    ->filter(fn (Product $product) =>
+                        $product->default_price->type === "recurring"
+                    )
+                    ->filter(fn (Product $product) =>
+                        !Package::query()
+                            ->where("id", "=", $product->id)
+                            ->exists()),
+            "selected" => $request->query("selected")
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request, StripeClient $stripe) {
+        $request->validate([
+            "productId" => "required"
+        ], [
+            "productId.required" => "Select an option"
+        ]);
+
+        try {
+            $stripeProduct = $stripe->products->retrieve($request->productId);
+            if (is_null($stripeProduct))
+                return back()->withErrors([
+                    "productId" => "Invalid product id"
+                ]);
+            if (Package::query()
+                ->where("id", "=", $stripeProduct->id)
+                ->exists())
+                return back()->withErrors([
+                    "productId" => "Package already exists"
+                ]);
+
+            // getting the price
+            $priceId = gettype($stripeProduct->default_price) === "string" ?
+                $stripeProduct->default_price :
+                $stripeProduct->id;
+            $stripePrice = $stripe->prices->retrieve($priceId);
+            if ($stripePrice === null)
+                return back()->withErrors([
+                    "productId" => "Stripe error on getting the price by id"
+                ]);
+            if ($stripePrice->recurring === null)
+                return back()->withErrors([
+                    "productId" => "Invalid product, it does not have recurring payment"
+                ]);
+
+            $price = Price::query()->findOrNew($priceId);
+            if (!Price::query()
+                ->where("id", "=", $price)
+                ->exists()) {
+                $price->id = $stripePrice->id;
+                $price->currency = $stripePrice->currency;
+                $price->unitAmount = $stripePrice->unit_amount;
+                $price->recurringInterval = $stripePrice->recurring["interval"];
+                if (!$price->save())
+                    return back()->withErrors([
+                        "productId" => "Failed to create Stripe price"
+                    ]);
+            }
+
+            $package = Package::create([
+                "id" => $stripeProduct->id,
+                "name" => $stripeProduct->name,
+                "description" => $stripeProduct->description,
+                "priceId" => $stripePrice->id
+            ]);
+
+            return redirect()->route("packages.show", [
+                "package" => $stripeProduct->id
+            ]);
+        } catch (ApiErrorException $error) {
+            return back()->withErrors([
+                "productId" => "Stripe error " . $error->getMessage()
+            ]);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Package $package)
-    {
-        //
+    public function show(Package $package) {
+        return view("packages.show", [
+            "package" => $package
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Package $package)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Package $package)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Package $package)
-    {
-        //
+    public function destroy(Package $package) {
+        $package->delete();
+        return redirect()->route("packages.index");
     }
 }
--- admin/app/Models/Package.php
+++ admin/app/Models/Package.php
@@ -10,4 +10,19 @@ class Package extends Model
     use HasFactory;
 
     protected $connection = 'orders';
+    protected $table = 'Package';
+    protected $casts = [
+        "id" => "string"
+    ];
+
+    const CREATED_AT = "createdAt";
+    const UPDATED_AT = "updatedAt";
+
+    protected $fillable = [
+        "id", "name", "description", "priceId"
+    ];
+
+    public function price() {
+        return $this->hasOne(Price::class, "priceId");
+    }
 }
