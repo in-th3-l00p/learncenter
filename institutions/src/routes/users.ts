588d74cc40416af1a5d5d62e798be229756f8bd0
in-th3-l00p <indianucode0303@gmail.com>
2024-03-08 06:11:52 +0200

    complete usage of institution service on institution users

--- institutions/src/routes/users.ts
+++ institutions/src/routes/users.ts
@@ -18,34 +18,18 @@ const router = express.Router();
 router.get(
     "/",
     authenticate(prisma, logger),
-    (req: UserRequest<UserDto>, res) => {
-        const { role } = req.query;
-
-        prisma.usersOnInstitutions.findMany({
-            where: {
-                userId: req.user!.id,
-                role: (
-                    role &&
-                    typeof role === "string" &&
-                    role in UserRole
-                )   ? role as UserRole
-                    : undefined
-            },
-            include: {
-                institution: true
-            }
-        })
-            .then(users => {
-                res.send(users);
-            })
-            .catch(err => {
-                logger.error("Error getting user institutions", err);
-                res.status(500).send({
-                    errors: [{
-                        msg: "Internal server error"
-                    }]
-                });
-            });
+    async (req: UserRequest<UserDto>, res) => {
+        try {
+            const {role} = req.query;
+            res.send(await institutionService.getUsers(
+                undefined,
+                req.user!.id,
+                typeof role === "string" ? role as UserRole : undefined
+            ));
+        } catch (err) {
+            logger.error("Error getting user institutions", err);
+            handleServiceError(res, err);
+        }
     });
 
 router.get(
@@ -55,23 +39,17 @@ router.get(
     authenticate(prisma, logger),
     institutionAccess,
     async (req: InstitutionRequest<UserDto>, res) => {
-        const users = await prisma.usersOnInstitutions.findMany({
-            where: {
-                institutionId: req.institution!.id,
-                userId: typeof req.query.userId === "string" ?
+        try {
+            res.send(await institutionService.getUsers(
+                req.institution!.id,
+                typeof req.query.userId === "string" ?
                     parseInt(req.query.userId) :
                     undefined
-            },
-            include: { user: true, institution: true }
-        });
-
-        return res.send(users.map(user => ({
-            id: user.id,
-            user: user.user,
-            institutionId: user.institutionId,
-            role: user.role,
-            createdAt: user.createdAt
-        })));
+            ));
+        } catch (err) {
+            logger.error("Error getting users from institution", err);
+            handleServiceError(res, err);
+        }
     });
 
 router.post(
@@ -94,6 +72,7 @@ router.post(
                 msg: "User invited to institution"
             });
         } catch (err) {
+            logger.error("Error inviting user to institution", err);
             handleServiceError(res, err);
         }
     });
@@ -148,53 +127,17 @@ router.delete(
             return res.status(400).send(errors);
 
         const { institution } = req;
-        const { userId } = matchedData(req);
-        const user = await prisma.user.findUnique({
-            where: { id: parseInt(userId) }
-        });
-        if (!user)
-            return res.status(404).send({
-                errors: [{
-                    msg: "User not found"
-                }]
+        const data = matchedData(req);
+        const userId = parseInt(data["userId"]);
+        try {
+            await institutionService.removeUser(institution!.id, userId);
+            res.send({
+                msg: "User removed from institution"
             });
-
-        const relation = await prisma.usersOnInstitutions.findFirst({
-            where: {
-                institutionId: institution!.id,
-                userId: user.id
-            }
-        });
-        if (!relation)
-            return res.status(404).send({
-                errors: [{
-                    msg: "User not found in institution"
-                }]
-            });
-
-        prisma.usersOnInstitutions.delete({
-            where: { id: relation.id }
-        })
-            .then(relation => {
-                if (!relation)
-                    return res.status(404).send({
-                        errors: [{
-                            msg: "User not found in institution"
-                        }]
-                    });
-                logger.info(`User removed from institution: ${JSON.stringify(relation)}.`);
-                res.send({
-                    msg: "User removed from institution"
-                });
-            })
-            .catch(err => {
-                logger.error("Error removing user from institution", err);
-                res.status(500).send({
-                    errors: [{
-                        msg: "Internal server error"
-                    }]
-                });
-            });
+        } catch (err) {
+            logger.error("Error removing user from institution", err);
+            handleServiceError(res, err);
+        }
     });
 
 export default router;
--- institutions/src/services/InstitutionService.ts
+++ institutions/src/services/InstitutionService.ts
@@ -3,24 +3,9 @@ import {ServiceError} from "types";
 import logger from "logger";
 import Amqp from "streaming";
 import {EventType} from "streaming/src/event";
+import {Prisma, UserRole} from "@prisma/client";
 
 class InstitutionService {
-    public async checkUserInInstitution(
-        userId: number,
-        institutionId: number
-    ) {
-        if (await prisma.usersOnInstitutions.count({
-            where: {
-                userId,
-                institutionId,
-                role: {
-                    notIn: ["PENDING", "BANNED", "DELETED"]
-                }
-            }
-        }) === 0)
-            throw new ServiceError(403, ["User not in institution"]);
-    }
-
     public async getInstitution(id: number, userId: number) {
         const institution = await prisma.institution.findUnique({
             where: { id }
@@ -180,10 +165,63 @@ class InstitutionService {
         return user;
     }
 
-    public async getUsers(institutionId: number) {
+    public async getUsers(
+        institutionId?: number,
+        userId?: number,
+        role?: UserRole
+    ) {
         return prisma.usersOnInstitutions.findMany({
-            where: {institutionId}
+            where: {
+                institutionId: institutionId,
+                userId: userId,
+                role: role
+            },
+            include: { user: true, institution: true }
         });
+    }
+
+    public async checkUserInInstitution(
+        userId: number,
+        institutionId: number
+    ) {
+        if (await prisma.usersOnInstitutions.count({
+            where: {
+                userId,
+                institutionId,
+                role: {
+                    notIn: ["PENDING", "BANNED", "DELETED"]
+                }
+            }
+        }) === 0)
+            throw new ServiceError(403, ["User not in institution"]);
+    }
+
+    public async removeUser(
+        institutionId: number,
+        userId: number
+    ) {
+        if (await prisma.usersOnInstitutions.count({
+            where: {
+                userId,
+                institutionId
+            }
+        }) === 0)
+            throw new ServiceError(404, ["User not in institution"]);
+
+        await prisma.usersOnInstitutions.deleteMany({
+            where: {
+                userId,
+                institutionId
+            }
+        });
+        Amqp.getInstance().publish({
+            type: EventType.USER_LEFT_INSTITUTION,
+            data: {
+                user: userId,
+                institution: institutionId
+            }
+        });
+        logger.info("Removed user " + userId + " from institution " + institutionId + ".");
     }
 }
 
