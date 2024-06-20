28f03213c9ad013f9121ab0b2503f249890e9516
in-th3-l00p <indianucode0303@gmail.com>
2024-06-20 18:35:52 +0300

    dashboard refactorization

---
+++ app/dashboard/list.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { Link } from "@nextui-org/link";
+import { tv } from "tailwind-variants";
+import clsx from "clsx";
+
+const list = tv({
+  base: clsx(
+    "flex gap-8 overflow-x-scroll no-scrollbar",
+    "bg-content2 p-8 rounded-xl",
+  ),
+});
+
+const listCard = tv({
+  base: "w-[300px] min-w-[300px] h-48",
+});
+
+const listCardTitle = tv({
+  base: "text-lg",
+});
+
+export function List({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      className={list()}
+      onWheel={(e) => {
+        e.preventDefault();
+        e.currentTarget.scrollLeft += e.deltaX;
+        e.currentTarget.scrollLeft += e.deltaY;
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function ListCard({
+  title,
+  description,
+  href,
+}: {
+  title: string;
+  description: string;
+  href: string;
+}) {
+  return (
+    <Card as={Link} className={listCard()} href={href}>
+      <CardHeader>
+        <h3 className={listCardTitle()}>{title}</h3>
+      </CardHeader>
+      <CardBody>
+        <p>
+          {description.length > 0
+            ? description
+            : "There is nothing written yet."}
+        </p>
+      </CardBody>
+    </Card>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,56 +1,14 @@
 import { getServerSession } from "next-auth";
-import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { Card, CardBody } from "@nextui-org/card";
 import { Button } from "@nextui-org/button";
 import { redirect } from "next/navigation";
-import { Link } from "@nextui-org/link";
-import clsx from "clsx";
-import { tv } from "tailwind-variants";
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import User from "@/models/User";
 import { subtitle, title } from "@/components/primitives";
 import Note, { INote } from "@/models/Note";
 import Node from "@/models/Node";
-
-const list = tv({
-  base: clsx(
-    "flex gap-8 overflow-x-scroll no-scrollbar",
-    "bg-content2 p-8 rounded-xl",
-  ),
-});
-
-const listCard = tv({
-  base: "w-[300px] min-w-[300px] h-48",
-});
-
-const listCardTitle = tv({
-  base: "text-lg",
-});
-
-function ListCard({
-  title,
-  description,
-  href,
-}: {
-  title: string;
-  description: string;
-  href: string;
-}) {
-  return (
-    <Card as={Link} className={listCard()} href={href}>
-      <CardHeader>
-        <h3 className={listCardTitle()}>{title}</h3>
-      </CardHeader>
-      <CardBody>
-        <p>
-          {description.length > 0
-            ? description
-            : "There is nothing written yet."}
-        </p>
-      </CardBody>
-    </Card>
-  );
-}
+import { List, ListCard } from "@/app/dashboard/list";
 
 export default async function Dashboard() {
   async function create() {
@@ -113,7 +71,7 @@ export default async function Dashboard() {
           </Button>
         </form>
 
-        <div className={list()}>
+        <List>
           {notes.map((note: INote, index) => (
             <ListCard
               key={index}
@@ -124,13 +82,17 @@ export default async function Dashboard() {
           ))}
 
           <form action={create}>
-            <Card as={Button} className={listCard()} type={"submit"}>
+            <Card
+              as={Button}
+              className={"w-[300px] min-w-[300px] h-48"}
+              type={"submit"}
+            >
               <CardBody className={"flex justify-center items-center"}>
                 <h3 className={"text-3xl"}>+</h3>
               </CardBody>
             </Card>
           </form>
-        </div>
+        </List>
       </div>
     </section>
   );
